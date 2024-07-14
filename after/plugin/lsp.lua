cce8178fe362e40d2c45d9abc01f4b3a10bf4cc7
anatoliiurianskyi <anatolii.urianskyi@intellias.com>
2024-07-14 22:37:27 +0300

    VIM_CONF-4 added lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -22,4 +22,11 @@ require('mason-lspconfig').setup({
     end,
   },
 })
-
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp_zero.defaults.cmp_mappings({
+	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+	['<C-y>'] = cmp.mapping.confirm({select = true}),
+	['<C-Space>'] = cmp.mapping.complete(),
+})
