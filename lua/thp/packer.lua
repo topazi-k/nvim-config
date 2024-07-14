2ec9e7133fe686cd7672cf8509381e619a3472ef
anatoliiurianskyi <anatolii.urianskyi@intellias.com>
2024-07-14 16:30:02 +0300

    VIM_CONF-2 added treesitter and harpoon

--- lua/thp/packer.lua
+++ lua/thp/packer.lua
@@ -22,4 +22,9 @@ return require('packer').startup(function(use)
   use({
 	  'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}
   })
+  
+  use({
+	 'nvim-treesitter/playground' 
+  })
+  use('theprimeagen/harpoon')
   end)
